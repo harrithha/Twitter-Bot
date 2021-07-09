973faf774af61a21a74c5d4cd915dc224ac40c3a
harrithha <76431379+harrithha@users.noreply.github.com>
2021-07-09 17:23:17 +0530

    Update iplapi.py

--- iplapi.py
+++ iplapi.py
@@ -1,14 +1,19 @@
 import requests
 from bs4 import BeautifulSoup
+init = 0
+    
+def live_scores():
+    final_print = []
+    final_print += ['-----Live Scores-----\n']
+    score_check = 0
+    
 
-def work():
     url = "https://www.espncricinfo.com/live-cricket-score"
     r = requests.get(url)
     htmlContent = r.content
     soup = BeautifulSoup(htmlContent, 'html.parser')
     live_check = soup.find_all("div", class_="match-info match-info-FIXTURES")
 
-    print('----Live Scores----')
 
     for i in live_check:
         statusRed = 0
@@ -17,20 +22,45 @@ def work():
                 statusRed += 1
         if statusRed == 1:
             teams_parent = i.find("div", class_="teams")
-            score = ""
+            score = []
             team_names = []
+            i=0
+            check_one = ''
+            check_two = ''
+            flag_one = 0
+            flag_two = 0
 
             teams = teams_parent.find_all("div", class_="team")
             for team in teams:
                 score_info = team.find("div", class_="score-detail")
-                if score_info and score == "":
-                    score = score_info.get_text()
+                if score_info and i == 0:
+                    score_value_A = team.find("span", class_="score").get_text().split("/")
+                   # print(score_value_A[0])
+                    score_check = score_value_A[0]
+                    score.append(score_info.get_text())
+                    check_one = 'Printed A Score'
+                    flag_one = 1
+                elif i == 0 and flag_one == 0 :
+                    check_one = 'Did Not Print A Score'
+                if score_info and i == 1:
+                    score.append(score_info.get_text())
+                    check_two = 'Printed B Score'
+                    flag_two = 1
+                elif i == 0 and flag_two == 0 :
+                    check_two = 'Did Not Print B Score'
                 team_Detail = team.find("div", class_="name-detail")
                 if team_Detail:
                     team_names.append(team_Detail.get_text())
-            if len(team_names) == 2:
-               print(team_names[0], 'Vs', team_names[1], score)
+                    i+=1
+            #print(check_one)
+            #print(check_two)
+            if len(team_names) == 2 and len(score) == 2:
+               final_print += (team_names[0], score[0],  'Vs', team_names[1], score[1],"\n")
+            elif len(team_names) == 2 and check_one == 'Printed A Score':
+                final_print += (team_names[0], score[0],  'Vs', team_names[1],"\n")
+            elif len(team_names) == 2 and check_two == 'Printed B Score':
+                final_print += (team_names[0],  'Vs', team_names[1], score[0], "\n")
             else:
                 print('Multiple teams Found For A Match')
+    return(' '.join(final_print))
 
-print("Hi")
